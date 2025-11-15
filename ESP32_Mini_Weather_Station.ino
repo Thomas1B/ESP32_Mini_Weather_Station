4a9f853d19a78aec426c71e37a85b0b901982662
Thomas1B <thomasbourgeois2@outlook.com>
2025-11-14 21:49:40 -0800

    Updated comments and readme

--- ESP32_Mini_Weather_Station.ino
+++ ESP32_Mini_Weather_Station.ino
@@ -1,6 +1,9 @@
 /*
   Mini Weather Station using ESP32C3 dev kit 2 and a BME280 Sensor.
 
+  This program was originally written using a esp32 C3 dev kit 2 so pins may be different.
+  It collects temperature, humidity, and pressure then uploads its reading to ThingSpeak for remote viewing.
+
   By Thomas Bourgeois
 */
 
@@ -51,12 +54,12 @@ void setup() {
     printf("\nWiFi connected!\n");
   }
 
-  // ThingSpeak.begin(client);
-
   // Checking BME status
   if (bme.begin(BME_ADR)) {
     printf("BME280 detected!\n");
   }
+
+  // ThingSpeak.begin(client); // connecting to thingspeak
 }
 
 void loop() {
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ESP32 Mini Weather Station with BME280
+
+  Mini Weather Station using ESP32C3 dev kit 2 and a BME280 Sensor.
+
+  This program was originally written using a esp32 C3 dev kit 2 so pins may be different.
+  It collects temperature, humidity, and pressure then uploads its reading to ThingSpeak for remote viewing.
+
+  By Thomas Bourgeois
