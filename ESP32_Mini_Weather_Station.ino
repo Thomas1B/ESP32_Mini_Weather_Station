dc731df18af34714921570ee07194fbc4063f48d
Thomas1B <thomasbourgeois2@outlook.com>
2025-11-15 13:50:17 -0800

    updated features for failed connections during set up

--- ESP32_Mini_Weather_Station.ino
+++ ESP32_Mini_Weather_Station.ino
@@ -1,11 +1,11 @@
-
 /*
   Mini Weather Station using ESP32C3 dev kit 2 and a BME280 Sensor.
 
   This program was originally written using a esp32 C3 dev kit 2 so pins may be different.
   It collects temperature, humidity, and pressure then uploads its reading to ThingSpeak for remote viewing.
 
-  Note:
+  LED Status:
+    LED -> Green:            Everything working
     LED -> Flashing RED:     Failed WiFi Connection
     LED -> Flashing Yellow:  Failed BME280 Connection
 
@@ -13,24 +13,23 @@
 */
 
 //  **************** Libraries ****************
+#include "LibPrintf.h"
 #include "secrets.h"
-#include "ThingSpeak.h"
 #include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "LibPrintf.h"
 #include <Adafruit_NeoPixel.h>
+#include "ThingSpeak.h"
 
 //  **************** User Variables ****************
+#define PIN_RGB 8
 #define I2C_SDA 1
 #define I2C_SCL 0
-#define PIN_RGB 8
-// #define BME_ADR 0x76  // bme address
-#define BME_ADR 0x90  // bme address test
+#define BME_ADR 0x76  // BME address
 
-const unsigned long wifiTimeout = 30 * 1000;
+const unsigned long wifiTimeout = 5 * 1000;  // must be milliseconds
 
-//  **************** Variables ****************
+//  **************** Program Variables ****************
 
 // WiFi Parameters:
 unsigned long wifiStartAttemptTime = millis();
@@ -42,7 +41,7 @@ WiFiClient client;
 unsigned long myChannelNumber = SECRET_CH_ID;
 const char* myWriteAPIKey = SECRET_WRITE_APIKEY;
 
-
+// RGB Object:
 Adafruit_NeoPixel rgbLED(1, PIN_RGB, NEO_GRB + NEO_KHZ800);
 Adafruit_BME280 bme;  // bme sensor object
 
@@ -51,27 +50,37 @@ void setRGB(byte r, byte g, byte b);
 
 //  **************** Set up ****************
 void setup() {
-  setRGB(0, 0, 0); // turnning of LED to start
+  setRGB(0, 0, 0);  // LED off to start
   Serial.begin(115200);
-  printf("***Program Started***\n");
+  printf("\n*** Program Started ***\n");
   delay(1000);
 
   // --- I2C ---
   Wire.begin(I2C_SDA, I2C_SCL);
 
   // Connecting to WiFi, timeout after some time.
-  printf("Connecting to WiFi: %s\n", ssid);
+  unsigned long prevTime = 0;
+  bool ledState = false;
+  printf("Connecting to %s", ssid);
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED && millis() - wifiStartAttemptTime < wifiTimeout) {
-    printf(".");
-    delay(250);
+    unsigned long now = millis();  // flashing led while connecting
+    if (now - prevTime >= 100) {
+      prevTime = now;
+      ledState = !ledState;
+      if (ledState) {
+        setRGB(50, 0, 0);
+      } else {
+        setRGB(0, 0, 0);
+      }
+    }
   }
 
   // Checking WiFi Status
   if (WiFi.status() == WL_CONNECTED) {
-    printf("\nWiFi connected!\n");
+    printf(" -> Connected!\n");
   } else {
-    printf("\nWiFi connection failed...\n");
+    printf(" -> Failed!\n");
     while (1) {
       setRGB(50, 0, 0);
       delay(500);
@@ -82,9 +91,9 @@ void setup() {
 
   // Checking BME status
   if (bme.begin(BME_ADR)) {
-    printf("BME280 detected!\n");
+    printf("BME280 Sensor Connection Succesful!\n");
   } else {
-    printf("\nBME280 connection failed...\n");
+    printf("BME280 Sencer Connection Cailed...\n");
     while (1) {
       setRGB(50, 50, 0);
       delay(500);
@@ -113,6 +122,14 @@ void loop() {
 
 
 void setRGB(byte r, byte g, byte b) {
+  /*
+  Function to set color and update rgb LED.
+
+  Parameters:
+    r: 0-255 value for red.
+    g: 0-255 value for green.
+    b: 0-255 value for blue.
+  */
   rgbLED.setPixelColor(0, rgbLED.Color(r, g, b));
   rgbLED.show();
 }
